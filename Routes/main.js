3464fc16e23755f9aa32c106530a61fa9daacc18
rach-con13 <rachaelconcessio+@gmail.com>
2021-03-08 21:41:04 -0500

    added pages

--- Routes/main.js
+++
@@ -1,7 +0,0 @@
-let router = require('express').Router();
-
-router.get('/',(req,res) => {
-    res.send('main express route')
-})
-
-module.exports = router;
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
