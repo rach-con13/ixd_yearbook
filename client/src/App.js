644c6f28a58709fae7f51ac330af456677152eb4
rach-con13 <rachaelconcessio+@gmail.com>
2021-03-08 21:08:58 -0500

    updated for vercel

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  let [data,setData] = useState(null);
+  // useEffect(() => {
+  //   let getResults = async() => {
+  //     let results = await axios.get('http://localhost:5000');
+  //     setData(results);
+  //   }
+  //   getResults();
+  // }, [data])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Testing to see if its served</p>
     </div>
   );
 }
